bd032329591421660ed1a18552277c9696e76d15
sashASSSSSSS <90263225+sashASSSSSSS@users.noreply.github.com>
2023-06-20 11:04:35 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,1 +1,17 @@
-12345
+a=input()
+b=len(a)
+x1=0
+x2=0
+for i in a:
+    if i=="o":
+        x1+=1
+    if i=="x":
+        x2+=1
+def pp(x1, x2):
+    if x1==x2:
+        return True
+    if x1!=x2:
+        return False
+print(pp(x1, x2))    
+        
+    
